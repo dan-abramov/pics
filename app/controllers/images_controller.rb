dde63606c3051af2a98c0162a6e9123592abc722
dan-abramov <mymindsgate@gmail.com>
2019-09-27 11:14:32 +0300

    user can upload image

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,20 @@
+class ImagesController < ApplicationController
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.create!(image_params)
+    redirect_to @image
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:title, :description, :image, :user_id)
+  end
+end
---
+++ db/migrate/20190925201412_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :images do |t|
+      t.string :title
+      t.text   :description
+      t.string :user_id, index: true
+      t.timestamps
+    end
+  end
+end
